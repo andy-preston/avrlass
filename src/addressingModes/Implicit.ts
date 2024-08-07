28dff9242552de9902b0ab4cf4691c574141fa58
Andy Preston <edgeeffect@gmail.com>
2024-08-07 18:50:22 +0100

    Some of the "implicit" instructions are "officially" in other modes

--- src/addressingModes/Implicit.ts
+++ src/addressingModes/Implicit.ts
@@ -4,19 +4,22 @@ import { checkCount } from "../instructions/operands.ts";
 
 const mappings: Record<string, string> = {
     "BREAK":  "1001_0101_1001_1000",
-    "EICALL": "1001_0101_0001_1001",
-    "EIJMP":  "1001_0100_0001_1001",
-    "ELPM":   "1001_0101_1101_1000",
-    "ICALL":  "1001_0101_0000_1001",
-    "IJMP":   "1001_0100_0000_1001",
-    "LPM":    "1001_0101_1100_1000",
     "NOP":    "0000_0000_0000_0000",
     "RET":    "1001_0101_0000_1000",
     "RETI":   "1001_0101_0001_1000",
     "SLEEP":  "1001_0101_1000_1000",
-    "SPM":    "1001_0101_1110_1000",
-    "SPM.Z+": "1001_0101_1111_1000",
     "WDR":    "1001_0101_1010_1000",
+    // Program Memory Constant Addressing
+    "ELPM":   "1001_0101_1101_1000",
+    "LPM":    "1001_0101_1100_1000",
+    "SPM":    "1001_0101_1110_1000",
+    // Program with Memory Post-increment
+    "SPM.Z+": "1001_0101_1111_1000",
+    // Indirect Program Addressing
+    "IJMP":   "1001_0100_0000_1001",
+    "EIJMP":  "1001_0100_0001_1001",
+    "ICALL":  "1001_0101_0000_1001",
+    "EICALL": "1001_0101_0001_1001",
 };
 
 export const encode = (
